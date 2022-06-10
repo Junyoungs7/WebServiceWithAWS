d5514443ffd5ccff0af59866851677e74bd55ef4
junyoung <junyoungs7@naver.com>
2022-06-10 14:02:38 +0900

    Initial commit

---
+++ src/main/java/Jy/WebServiceWithAWS/WebServiceWithAwsApplication.java
@@ -0,0 +1,13 @@
+package Jy.WebServiceWithAWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceWithAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceWithAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Jy/WebServiceWithAWS/WebServiceWithAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package Jy.WebServiceWithAWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceWithAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
