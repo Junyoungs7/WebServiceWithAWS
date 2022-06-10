83a447a652ff414a38b5de4a352ac717084e8996
junyoung <junyoungs7@naver.com>
2022-06-10 18:18:51 +0900

    22-06-10

---
+++ src/test/java/Jy/WebServiceWithAWS/web/HelloControllerTest.java
@@ -0,0 +1,43 @@
+package Jy.WebServiceWithAWS.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc Mvc;
+
+    @Test
+    public void hello_return() throws Exception{
+        String hello = "hello";
+
+        Mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto_return() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        Mvc.perform(get("/hello/dto").param("name", name).param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", equalTo(name)))
+                .andExpect(jsonPath("$.amount", equalTo(amount)));
+
+    }
+
+
+}
---
+++ src/test/java/Jy/WebServiceWithAWS/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,26 @@
+package Jy.WebServiceWithAWS.web.dto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloResponseDtoTest {
+
+    @Test
+    public void lombok_func_test(){
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+
+    }
+
+}
