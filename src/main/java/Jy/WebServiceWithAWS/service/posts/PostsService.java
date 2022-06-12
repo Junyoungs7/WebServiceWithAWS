afdf2ba7391e45b419ecaf922fa2d6be7b149813
junyoung <junyoungs7@naver.com>
2022-06-12 20:10:15 +0900

    22-06-12

--- src/main/java/Jy/WebServiceWithAWS/service/posts/PostsService.java
+++ src/main/java/Jy/WebServiceWithAWS/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package Jy.WebServiceWithAWS.service.posts;
 
 import Jy.WebServiceWithAWS.domain.posts.Posts;
 import Jy.WebServiceWithAWS.domain.posts.PostsRepository;
+import Jy.WebServiceWithAWS.web.dto.PostsListResponseDto;
 import Jy.WebServiceWithAWS.web.dto.PostsResponseDto;
 import Jy.WebServiceWithAWS.web.dto.PostsSaveRequestDto;
 import Jy.WebServiceWithAWS.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,5 +34,19 @@ public class PostsService {
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("no content. id=" + id));
+        postsRepository.delete(posts);
+
     }
 }
