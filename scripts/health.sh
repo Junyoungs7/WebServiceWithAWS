3a2d32cce99345848d6e783ed3a54aa3da6f52b0
junyoung <junyoungs7@naver.com>
2022-07-01 16:47:11 +0900

    22-07-01

--- scripts/health.sh
+++ scripts/health.sh
@@ -18,13 +18,13 @@ do
   UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
 
   if [ ${UP_COUNT} -ge 1 ]
-  then
-    echo "> Health check 성공"
-    switch_proxy
-    break
+  then # $up_count >= 1 ("real" 문자열이 있는지 검증)
+      echo "> Health check 성공"
+      switch_proxy
+      break
   else
-    echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
-    echo "> Health check: ${RESPONSE}"
+      echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
+      echo "> Health check: ${RESPONSE}"
   fi
 
   if [ ${RETRY_COUNT} -eq 10 ]
@@ -34,6 +34,6 @@ do
     exit 1
   fi
 
-  echo "> Health check 연결 실패. 재시도.."
+  echo "> Health check 연결 실패. 재시도..."
   sleep 10
 done
